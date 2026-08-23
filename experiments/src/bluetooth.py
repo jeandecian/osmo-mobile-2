f31d7bf29d6efee305b142bac7fc7a3b9ce969b6
Jean Decian <jeandecian@hotmail.fr>
2026-08-23 12:35:51 -0400

    feat(bluetooth): enhance device discovery by adding target device identification

--- experiments/src/bluetooth.py
+++ experiments/src/bluetooth.py
@@ -16,14 +16,30 @@ class Bluetooth:
         return await BleakScanner.discover(timeout=self.timeout)
 
 
+def find_target_device(devices: list[BLEDevice]) -> BLEDevice | None:
+    """Finds a target device from a list of BLE devices."""
+
+    for device in devices:
+        device_name: str = (device.name or "").lower()
+
+        if "dji" in device_name or "osmo" in device_name:
+            return device
+
+    return None
+
+
 async def main() -> None:
     bt: Bluetooth = Bluetooth()
 
     print("Scanning for BLE devices...")
     devices: list[BLEDevice] = await bt.scan()
+    target_device: BLEDevice | None = find_target_device(devices)
 
-    for device in devices:
-        print(device)
+    if not target_device:
+        print("No target device found.")
+        return
+
+    print(f"Found target device: {target_device}")
 
 
 if __name__ == "__main__":
